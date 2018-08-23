ae74ec9d7c23d53a155625e026e2a9da127f9f84
Kevin Tran <ktran050@ucr.edu>
2018-08-23 14:55:03 -0700

    final setup for signup validation. bug fixing

--- src/app/modules/signup/signup.component.ts
+++ src/app/modules/signup/signup.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+// import { RouterModule } from '@angular/router'; // may or may not be necessary
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css', './_signup.component.scss']
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  codeValidity: boolean = false;
+  databaseValidated: boolean = false;
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  setCodeValidity(x) {
+  	if( x > 0 ) { this.codeValidity = true; }
+  	else{ this.codeValidity = false; }
+  }
+
+  getCodeValidity() {
+  	return this.codeValidity;
+  }
+
+  setDatabaseValidated() {
+    this.databaseValidated = !this.databaseValidated;
+    console.log(this.databaseValidated);
+  }
+  // Sets databaseValidated to true if user clicks next after the validation form is shown
+  // Sets databaseValidated to false if users return to the validation form
+  // *If extending the sign up form to more pages consider replacing this system
+
+  getDatabaseValidated() {
+    return this.databaseValidated;
+  }
+
+  checkCode(SignUpCode){
+  	if(SignUpCode == "a"){ this.setCodeValidity(true); }
+  	else{ this.setCodeValidity(false); }
   }
 
 }
