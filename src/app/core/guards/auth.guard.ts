082206bc42112a9b9ca4b5cf6a2598a63240ec18
Unknown <tdiaz@hmc.edu>
2019-08-17 03:01:29 -0400

    test

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -14,38 +14,38 @@ export class AuthGuard implements CanActivate {
 
 	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 		console.log(state.url);
-		if(!this.auth.isLoggedIn())
-		{
-			// router.navigate(['/login']);
-			// window.alert("You must be logged in");
-			if(state.url != '/login' && state.url != '/signup') {
-				this.router.navigate(['/login']);
-				return false;
-			}
-			return true;	// YES you're allowed to access /login if you're not logged in
-		}
-
-		const user = this.auth.getUser();
-
-		if(!user) return false;
-
-		if(state.url=='/login' || state.url=='/signup') {
-			this.router.navigate(['']);
-			return false;	// ur already logged in
-		}
-		
-		
-		if(state.url=='/mrfs')
-		{
-			if(user.access.club > 0) return true;
-			else {
-				window.alert("You do not have permission to view this");
-				this.router.navigate(['']);
-				return false;
-			}
-		}
-		if(state.url=='/cerfs')
-				return true;
+		// if(!this.auth.isLoggedIn())
+		// {
+		// 	// router.navigate(['/login']);
+		// 	// window.alert("You must be logged in");
+		// 	if(state.url != '/login' && state.url != '/signup') {
+		// 		this.router.navigate(['/login']);
+		// 		return false;
+		// 	}
+		// 	return true;	// YES you're allowed to access /login if you're not logged in
+		// }
+		//
+		// const user = this.auth.getUser();
+		//
+		// if(!user) return false;
+		//
+		// if(state.url=='/login' || state.url=='/signup') {
+		// 	this.router.navigate(['']);
+		// 	return false;	// ur already logged in
+		// }
+		//
+		//
+		// if(state.url=='/mrfs')
+		// {
+		// 	if(user.access.club > 0) return true;
+		// 	else {
+		// 		window.alert("You do not have permission to view this");
+		// 		this.router.navigate(['']);
+		// 		return false;
+		// 	}
+		// }
+		// if(state.url=='/cerfs')
+		// 		return true;
 		return true;	// should definitely default false, but need to figure out how to authorize '/cerf/:id'
 	}
 }
