eac0b0f42cd67d979a4e2ddfc49f46d971e8c3a3
zvidmarb <50197931+zvidmarb@users.noreply.github.com>
2020-09-17 20:47:29 -0700

    Asserting that End Time comes after Start Time and vice-versa (#18)
    * Uncommented code on cerf.comment.html pertaining to CERF categories
    
    * Added flex container to categories to properly display buttons, added spacing between
    
    * Implemented newLabel in order to add new categories (bug: not preservered across execution)
    
    * Fixed delete category 'x' not showing up, added a couple of checks for various category methods, cleaned up margins
    
    * Added escape key functionality for adding categories, cursor autofocuses on input (might need alternative?)
    
    * Cleaned up unnecessary files that were previously committed
    
    * Checked out json files from development
    
    * Fixed use of splice on category arrays
    
    * Created custom validation function for time range check, added it to time group... needs more revisions to function properly
    
    * Incomplete validator functionality, committing progress to work on alternate solution
    
    * Commented out original implementation, implemented MatDatePicker time restriction
    
    * Minor revisions and cleaning up code
    
    * Removed unintended imports that was throwing errors

--- src/app/modules/cerf/cerf.component.ts
+++ src/app/modules/cerf/cerf.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Directive, Renderer2, ElementRef, ViewChild, ViewChildren, QueryList } from '@angular/core';
 // import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
-import { FormGroup, FormControl, FormBuilder, FormArray, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { FormGroup, FormGroupDirective, FormControl, FormBuilder, FormArray, Validators, ValidatorFn, AbstractControl, Validator, ValidationErrors } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { Location } from '@angular/common';
@@ -19,8 +19,36 @@ import { CerfService, MemberService } from '@core/services';
 
 import { Observable, BehaviorSubject, zip } from 'rxjs';
 
+
+/**** Asserting End Time before Start Time Validator -Zeven VB ***********************
+   Commented out code: cerf.component.ts, cerf.component.html; 
+   core.module.ts, index.ts might be optional?
+	-(Search for invalid-range, invalidRangeValidator, timeErrorMatcher)
+	-Deleted html for ErrorStateMatcher
+
+import { ErrorStateMatcher } from '@angular/material/core';
+
+
+export class ParentErrMatcher implements ErrorStateMatcher {
+	isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+	  const invalidCtrl = !!(control && control.invalid);
+	  const invalidParent = !!(control && control.parent && control.parent.invalid);
+  
+	  return (invalidCtrl || invalidParent);
+	}
+  }
+
+export const invalidRangeValidator: ValidatorFn = (control: FormGroup) : ValidationErrors | null =>
+  {
+	const end = control.get('end');
+	const start = control.get('start');
+	return (end && start && end.value < start.value) ? {'invalid-range': true} : null;
+  }
+****************************************************************************************/ 
+  
 type Attendee = {name: string, service: number, leadership: number, fellowship: number};
 // type Member = {name: string, email: string, _id: string};	// Put into Member interface to be used by MemberService too
+
 
 @Component({
 	selector: 'app-cerf',
@@ -28,6 +56,7 @@ type Attendee = {name: string, service: number, leadership: number, fellowship: 
 	styleUrls: ['./cerf.component.css', './_cerf.component.scss'],
 	providers: [ CerfService ]
 })
+
 export class CerfComponent {
 
 	cerfId: string;
@@ -36,6 +65,9 @@ export class CerfComponent {
 
 	openedPanels: number[] = [0, 0, 0, 0, 0, 0];
 
+	maxStartTime: Date; 
+	minEndTime: Date;
+	
 	attendees;
 	attendanceColumns = ['members', 'service', 'leadership', 'fellowship'];
 	kfamColumns = ['org', 'numAttendees'];
@@ -137,6 +169,27 @@ export class CerfComponent {
 	ngAfterViewInit() {
 
 	}
+
+	//timeErrorMatcher = new ParentErrMatcher();
+
+	assertDateRange(event, fromStartTime: boolean) {
+		
+		if ( !this.minEndTime && !this.maxStartTime) {
+			if (fromStartTime) {
+				this.minEndTime = event.value;
+			} else {
+				this.maxStartTime = event.value;
+			}
+		} else {
+			if (this.minEndTime && fromStartTime) {
+				this.minEndTime = event.value;
+			} else if (this.maxStartTime && !fromStartTime){
+				this.maxStartTime = event.value;
+			}
+		}
+	}
+
+
 
 	// inputListReady(name, event) {
 	// 	this.cerfForm.setControl(name, event);
@@ -449,7 +502,7 @@ export class CerfComponent {
 			name: [model.name],
 			chair_id: [model.chair._id],
 			author: [model.author.name.first + " " + model.author.name.last],	// could create a name concatenator function...
-			time: this.builder.group(model.time),
+			time: this.builder.group(model.time /*, {validators: invalidRangeValidator}*/),
 			location: model.location,
 			contact: model.contact,
 			tags: this.builder.array(model.tags),
